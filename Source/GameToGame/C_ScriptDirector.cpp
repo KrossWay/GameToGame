25aba0b274049a30a2fd4f04e36508c3a45b6e42
Maxim <mahno32@rambler.ru>
2021-04-02 02:41:49 +0700

    Enable test json loading function again

--- Source/GameToGame/C_ScriptDirector.cpp
+++ Source/GameToGame/C_ScriptDirector.cpp
@@ -248,7 +248,7 @@ AC_ScriptDirector::AC_ScriptDirector()
     // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = true;
 
-    //ParseExample();
+    ParseExample();
 }
 
 void AC_ScriptDirector::Tick(float DeltaTime)
