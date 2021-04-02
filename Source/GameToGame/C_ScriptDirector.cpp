9863774e82729348cf3095ac3611a629568a80d9
Maxim <mahno32@rambler.ru>
2021-04-02 15:06:14 +0700

    Finished with json loading:
    - Make json loadable to the internal structure
    - Restructurize json script file: move Character upper

--- Source/GameToGame/C_ScriptDirector.cpp
+++ Source/GameToGame/C_ScriptDirector.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "C_ScriptDirector.h"
 #include <Templates/SharedPointer.h>
 #include <Json.h>
@@ -13,143 +11,120 @@ static const FString LEAF_TEXT_KEY("Text");
 static const FString LEAF_SPEAKER_KEY("Speaker");
 static const FString LEAF_SOUND_KEY("Sound");
 static const FString LEAF_EMOTION_KEY("Emotion");
-static const FString LEAF_ACTIONS_KEY("Actions");
 
 typedef TSharedRef<FJsonObject> JsonObjectRef;
 typedef TSharedPtr<FJsonObject> JsonObjectPtr;
+typedef TSharedRef<FJsonValue> JsonValueRef;
+typedef TSharedPtr<FJsonValue> JsonValuePtr;
 
 typedef TArray<FString> Actions_t;
+typedef TSharedPtr<Actions_t> Actions_p;
 // Last bunch of data and instructions in a script
 struct ScriptLeaf_s
 {
-    FString Text;
-    FString Speaker;
-    FString Sound;
-    FString Emotion;
-    Actions_t Actions;
+    FString text;
+    FString speaker;
+    FString sound;
+    FString emotion;
+    Actions_p actions;
 };
-typedef TSharedPtr<ScriptLeaf_s> ScriptLeaf_t;
+typedef TSharedPtr<ScriptLeaf_s> ScriptLeaf_p;
 
-typedef TArray<ScriptLeaf_t> Dialog_t;
-typedef TArray<ScriptLeaf_t> Answers_t;
+typedef TArray<ScriptLeaf_p> Dialog_t;
+typedef TArray<ScriptLeaf_p> Answers_t;
 
 struct Condition_s
 {
     Dialog_t dialog;
     Answers_t answers;
-    Actions_t actions;
+    Actions_p actions;
 };
-typedef TSharedPtr<Condition_s> Condition_t;
+typedef TSharedPtr<Condition_s> Condition_p;
 
-typedef TSharedPtr<TMap<FString, Condition_t>> CardObject_t;
-typedef TSharedPtr<TMap<FString, CardObject_t>> Act_t;
-typedef TSharedPtr<TMap<FString, Act_t>> Script_t;
+typedef TMap<FString, Condition_p> CardObject_t;
+typedef TSharedPtr<CardObject_t> CardObject_p;
+typedef TMap<FString, CardObject_p> Act_t;
+typedef TSharedPtr<Act_t> Act_p;
+typedef TMap<FString, Act_p> Script_t;
+typedef TSharedPtr<Script_t> Script_p;
 
 
-Actions_t load_actions(JsonObjectRef json_leaf)
+Actions_p load_actions(JsonObjectRef json_leaf)
 {
+    ULOG(Log, "Loading Actions.");
     const TArray<TSharedPtr<FJsonValue>>* json_values_array;
-    Actions_t actions;
+    Actions_p actions = MakeShared<Actions_t>();
     if (json_leaf->TryGetArrayField(ACTIONS_KEY, json_values_array))
     {
         check(json_values_array);
         for (auto json_value : *json_values_array)
-            actions.Add(json_value->AsString());
+            actions->Add(json_value->AsString());
     }
     return actions;
 }
 
-ScriptLeaf_t load_leaf(JsonObjectRef json_leaf)
+ScriptLeaf_p load_leaf(JsonObjectRef json_leaf)
 {
     ULOG(Log, "Restoring a Script leaf");
 
-    ScriptLeaf_t leaf;
+    ScriptLeaf_p leaf = MakeShared<ScriptLeaf_s>();
     auto json_leaf_dict = json_leaf->Values;
 
-    json_leaf->TryGetStringField(LEAF_TEXT_KEY, leaf->Text);
-    json_leaf->TryGetStringField(LEAF_SPEAKER_KEY, leaf->Speaker);
-    json_leaf->TryGetStringField(LEAF_SOUND_KEY, leaf->Sound);
-    json_leaf->TryGetStringField(LEAF_EMOTION_KEY, leaf->Emotion);
-
-
-    //if ()
-    //{
-    //    check(json_values_array);
-    //    for (auto json_value : *json_values_array)
-    //        dialog.Add(load_leaf(json_value->AsObject().ToSharedRef()));
-    //}
+    json_leaf->TryGetStringField(LEAF_TEXT_KEY, leaf->text);
+    json_leaf->TryGetStringField(LEAF_SPEAKER_KEY, leaf->speaker);
+    json_leaf->TryGetStringField(LEAF_SOUND_KEY, leaf->sound);
+    json_leaf->TryGetStringField(LEAF_EMOTION_KEY, leaf->emotion);
+    leaf->actions = load_actions(json_leaf);
     
     ULOG(Log, "Restoring a Script leaf finished");
     return leaf;
 }
 
-Condition_t load_condition(JsonObjectRef json_conditions)
+Condition_p load_condition(JsonObjectRef json_conditions)
 {
     ULOG(Log, "Restoring a Condition");
 
-    Condition_t condition;
+    Condition_p condition = MakeShared<Condition_s>();
+    const TArray<TSharedPtr<FJsonValue>>* json_values_array;
 
-    for (auto json_condition : json_conditions->Values)
+    if (json_conditions->TryGetArrayField(DIALOG_KEY, json_values_array))
     {
-        ULOG(Log, "Key found: %s", *json_condition.Key);
-
-        //auto tmp_obj = json_condition_val.Value->AsObject();
-        //if (tmp_obj)
-        //{
-        //    auto act = load_condition(tmp_obj.ToSharedRef());
-        //    card_objects->Add(json_condition_val.Key, act);
-        //}
-        //else
-        //    ULOG(Error, "Error while loading Card object.");
-
-        auto& json_condition_val = json_condition.Value->AsObject();
-        const TArray<TSharedPtr<FJsonValue>>* json_values_array;
-
-        if (json_condition_val->TryGetArrayField(DIALOG_KEY, json_values_array))
+        ULOG(Log, "Loading Dialog.");
+        check(json_values_array);
+        for (auto json_value : *json_values_array)
         {
-            check(json_values_array);
-            for (auto json_value : *json_values_array)
-            {
-                auto tmp_json_obj = json_value->AsObject();
-                if (tmp_json_obj)
-                    condition->dialog.Add(load_leaf(tmp_json_obj.ToSharedRef()));
-                else
-                    ULOG(Error, "Error while loading Dialog.");
-            }
-        }
-
-        if (json_condition_val->TryGetArrayField(ANSWERS_KEY, json_values_array))
-        {
-            check(json_values_array);
-            for (auto json_value : *json_values_array)
-            {
-                auto tmp_json_obj = json_value->AsObject();
-                if (tmp_json_obj)
-                    condition->answers.Add(load_leaf(tmp_json_obj.ToSharedRef()));
-                else
-                    ULOG(Error, "Error while loading Dialog.");
-            }
-        }
-
-        if (json_condition_val->TryGetArrayField(ACTIONS_KEY, json_values_array))
-        {
-            check(json_values_array);
-
-            for (auto json_value : *json_values_array)
-            {
-                condition->actions.Add(json_value->AsString());
-                check(condition->actions.Last().Len());
-            }
+            auto tmp_json_obj = json_value->AsObject();
+            if (tmp_json_obj)
+                condition->dialog.Add(load_leaf(tmp_json_obj.ToSharedRef()));
+            else
+                ULOG(Error, "Error while loading Dialog.");
         }
     }
+
+    if (json_conditions->TryGetArrayField(ANSWERS_KEY, json_values_array))
+    {
+        ULOG(Log, "Loading Answers.");
+        check(json_values_array);
+        for (auto json_value : *json_values_array)
+        {
+            auto tmp_json_obj = json_value->AsObject();
+            if (tmp_json_obj)
+                condition->answers.Add(load_leaf(tmp_json_obj.ToSharedRef()));
+            else
+                ULOG(Error, "Error while loading Answers.");
+        }
+    }
+
+    condition->actions = load_actions(json_conditions);
+
     return condition;
 }
 
-CardObject_t load_card_object(JsonObjectRef json_card_object)
+CardObject_p load_card_object(JsonObjectRef json_card_object)
 {
     ULOG(Log, "Restoring a Card object");
 
-    CardObject_t card_objects;
+    CardObject_p card_objects = MakeShared<CardObject_t>();
 
     for (auto json_condition_val : json_card_object->Values)
     {
@@ -168,11 +143,11 @@ CardObject_t load_card_object(JsonObjectRef json_card_object)
     return card_objects;
 }
 
-Act_t load_act(JsonObjectRef json_act)
+Act_p load_act(JsonObjectRef json_act)
 {
     ULOG(Log, "Restoring an Act");
 
-    Act_t card_objects;
+    Act_p card_objects = MakeShared<Act_t>();
 
     for (auto json_card_val : json_act->Values)
     {
@@ -191,7 +166,7 @@ Act_t load_act(JsonObjectRef json_act)
     return card_objects;
 }
 
-Script_t load_script(JsonObjectRef json_root)
+Script_p load_script(JsonObjectRef json_root)
 {
     ULOG(Log, "Loading a Script");
     //FString ExampleString = json_root->GetStringField("exampleString");
@@ -204,7 +179,7 @@ Script_t load_script(JsonObjectRef json_root)
     //    ULOG(Warning, "Key: %s", *key);
     //}
 
-    Script_t script;
+    Script_p script = MakeShared<Script_t>();
 
     for (auto json_act_val : json_root->Values)
     {
@@ -224,7 +199,7 @@ Script_t load_script(JsonObjectRef json_root)
     return script;
 }
 
-void ParseExample()
+Script_p ParseExample()
 {
     ULOG(Log, "Enter the ParseExample()");
 
@@ -242,12 +217,13 @@ void ParseExample()
 
     if (FJsonSerializer::Deserialize(json_reader, json_object) && json_object.IsValid())
     {
-        load_script(json_object.ToSharedRef());
+        return load_script(json_object.ToSharedRef());
     }
     else
     {
         ULOG(Error, "Unable to load script from file \"%s\"\nJson error messsage:\"%s\"", *script_full_path, *json_reader->GetErrorMessage());
     }
+    return Script_p(nullptr);
 }
 
 AC_ScriptDirector::AC_ScriptDirector()
@@ -255,7 +231,8 @@ AC_ScriptDirector::AC_ScriptDirector()
     // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = true;
 
-    //ParseExample();
+    auto script = ParseExample();
+    auto act1 = script.Get()['Act1'];
 }
 
 void AC_ScriptDirector::Tick(float DeltaTime)
